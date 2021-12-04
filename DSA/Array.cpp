e9f53c0be607b72b03ce27ee3d44f6be1f551b24
Sathish Routhu <93826731+sathishrouthu@users.noreply.github.com>
2021-12-05 01:37:43 +0530

    Create Array.cpp

---
+++ DSA/Array.cpp
@@ -0,0 +1,23 @@
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define sathish ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long int;
+#define nl cout<<endl;
+void solve()
+{
+
+}
+
+int main(){
+    sathish
+    int t=1;
+    //cin >> t;
+    while(t--){
+    	solve();
+    }
+    return 0;
+}
