5bed224114d008834a1354fcf4943c31dafc3848
Sathish Routhu <93826731+sathishrouthu@users.noreply.github.com>
2022-01-13 21:28:09 +0530

    Create Contain_duplicate.cpp

---
+++ LeetCode/Contain_duplicate.cpp
@@ -0,0 +1,19 @@
+/*
+
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Example 1:
+Input: nums = [1,2,3,1]
+Output: true
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: false
+
+
+*/
+bool containsDuplicate(vector<int>& nums) {
+  std::unordered_set<int> s(nums.begin(), nums.end());        //converting vector to set
+  return s.size() != nums.size();                              // if set and vector are same size the there is no duplicates else there is a duplicate
+  }
+
