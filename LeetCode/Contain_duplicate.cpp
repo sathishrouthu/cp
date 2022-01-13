c324a5c5b6bb3f00dd9836f96f0dfaa117ace8a5
Sathish Routhu <93826731+sathishrouthu@users.noreply.github.com>
2022-01-13 21:32:12 +0530

    Update Contain_duplicate.cpp

--- LeetCode/Contain_duplicate.cpp
+++ LeetCode/Contain_duplicate.cpp
@@ -12,8 +12,28 @@ Output: false
 
 
 */
+
+bool containsDuplicate(vector<int>& nums) 
+    {
+        std::sort(nums.begin(), nums.end());
+        
+        for(int i = 0;i < nums.size() - 1; i++)
+        {
+            if(nums.at(i) == nums.at(i + 1))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+/*
+
 bool containsDuplicate(vector<int>& nums) {
   std::unordered_set<int> s(nums.begin(), nums.end());        //converting vector to set
   return s.size() != nums.size();                              // if set and vector are same size the there is no duplicates else there is a duplicate
   }
 
+
+*/
