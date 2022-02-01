a59ca58491682ab7d3817aaa21c6810c2576dd18
Sathish Routhu <93826731+sathishrouthu@users.noreply.github.com>
2022-02-01 18:27:00 +0530

    Update Algo - 1.cpp

--- Algo - 1.cpp
+++ Algo - 1.cpp
@@ -15,3 +15,52 @@
         }
         return -1;
     }
+
+
+
+// TWO-POINTER technique 
+
+Ex: Two Sum problem.:
+for Example if we want to find a pair of elements in a given array those sums upto specific value
+Ex: [4,1,3,2,5,1,7] ,  value= 10
+
+  ans : 3,7
+    
+this problem take O(n^2) in a bruteforce approach.
+this can be solved in efficient way as O(n) by using a hashmap.
+
+    there is also another approach called twopointer technique.
+    [4,1,3,2,5,1,7]
+    before traversing our array we need to sort our array.
+    
+    [1,1,2,3,4,5,7]
+    
+    we use two pointer to traverse the array.
+    i=0 , j=n-1
+    
+    we traverse one element in the array from front by using i and back by using j
+   
+    when i==j array is exceeded.
+    when arr[i]+arr[j] == value
+      we return True
+    if arr[i]+arr[j] > value: we need to decrease the sum so we decrement j (j traversing bigger elements)
+    else  we need to increase sum by incrementing i
+    
+    
+    
+   1. Sort the array 
+   2.i=0 ,  j=n-1
+   3. while(i<j)
+    {
+      if (arr[i]+arr[j]== val)
+        return True;
+      else if(arr[i]+arr[j]>val)
+        j--;
+      else
+        i++;
+    }
+  4. return False;
+    
+    
+    
+    
