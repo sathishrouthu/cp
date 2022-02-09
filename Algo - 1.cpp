1a4c6b785b1691b2b34c884e6b4be7d66afa452d
Sathish Routhu <93826731+sathishrouthu@users.noreply.github.com>
2022-02-09 06:27:01 +0530

    Update Algo - 1.cpp

--- Algo - 1.cpp
+++ Algo - 1.cpp
@@ -60,7 +60,67 @@ this can be solved in efficient way as O(n) by using a hashmap.
         i++;
     }
   4. return False;
-    
-    
-    
+
+
+
+/*
+
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node. 
+
+Example 1:
+          __
+1--->2--->3--->4--->5
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+
+Explanation: The middle node of the list is node 3.
+
+Example 2:
+              ___
+1--->2--->3--->4--->5--->--->6
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 100].
+1 <= Node.val <= 100
+
+*//*
+This problem can be done in 2 ways :
+1.
+Put every node into an array A in order. Then the middle node is just A[A.length // 2], since we can retrieve each node by index.
+We can initialize the array to be of length 100, as we're told in the problem description that the input contains between 1 and 100 nodes.
+
+  */
+   
+    ListNode* middleNode(ListNode* head) {
+        ListNode *temp= head;
+        vector<ListNode*> arr={head};
+        while(arr.back()->next!=NULL)
+            arr.push_back(arr.back()->next);               
+        return arr[arr.size()/2];
+    }
+
+/*
+2.
+slow and fast pointer :
+When traversing the list with a pointer slow, make another pointer fast that traverses twice as fast.
+When fast reaches the end of the list, slow must be in the middle.
+*/
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL and fast->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
     
