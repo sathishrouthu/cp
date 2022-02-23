94ec4efe10f314a8bceb0d062220e77b6f75c05a
Sathish Routhu <93826731+sathishrouthu@users.noreply.github.com>
2022-02-23 22:43:07 +0530

    Update Subsequences.cpp

--- Recursions/Subsequences.cpp
+++ Recursions/Subsequences.cpp
@@ -1,7 +1,8 @@
 // Print all sub sequences of a given array
 // get every contingous or non contiguous sequence which follows the order of given array
 //
-
+#include<bits/stdc++.h>
+using namespace std;
 void subs(int i,vector<int>& ds,vector<int> arr,vector<vector<int>>& ans)
 {
   if(i==arr.size())
@@ -9,17 +10,19 @@ void subs(int i,vector<int>& ds,vector<int> arr,vector<vector<int>>& ans)
     ans.push_back(ds);
     return ;
   }
-  subs(i+1,ds,arr,ans)
-  ds.pop_back()
-  subs(i+1,ds,arr,ans)
+  ds.push_back(arr[i]);
+  subs(i+1,ds,arr,ans);
+  ds.pop_back();
+  subs(i+1,ds,arr,ans);
 }
 
 int main()
 {
-  vector<int> arr={2,1,3,4};
+  vector<int> arr={2,1,3};
   vector<int> ds;
+  vector<vector<int>> ans;
   subs(0,ds,arr,ans);
-  for(auto i : ans)
+  for(vector<int> i : ans)
   {
     for(int j : i)
     {
@@ -27,4 +30,5 @@ int main()
     }
     cout<<endl;
   }
+  return 0;
 }
