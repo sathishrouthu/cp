2d95381209ab558a9f06e9a3d923dc4f10da54a2
Sathish Routhu <93826731+sathishrouthu@users.noreply.github.com>
2022-03-15 14:18:51 +0530

    Create ValidPar.cpp

---
+++ LeetCode/ValidPar.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<int> stac;
+        string ans="";
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='('){
+                stac.push(i);
+                }
+            else if(s[i]==')'){
+                if(stac.empty()){
+                   s[i]='*';
+                   stac.pop();
+                }
+            }
+        }
+        while(!stac.empty()){
+            s[stac.top()]='*';
+            stac.pop();
+        }
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='*')
+                continue;
+            else
+                ans+=s[i];
+        }
+        return ans;
+    }
+};
