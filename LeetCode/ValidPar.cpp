da0ba8e2c941569662b0dd585750af684596df66
Sathish Routhu <93826731+sathishrouthu@users.noreply.github.com>
2022-03-15 15:18:00 +0530

    Update ValidPar.cpp

--- LeetCode/ValidPar.cpp
+++ LeetCode/ValidPar.cpp
@@ -1,5 +1,29 @@
-class Solution {
-public:
+
+/*
+
+Given a string s of '(' , ')' and lowercase English characters.
+
+Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+
+Formally, a parentheses string is valid if and only if:
+
+It is the empty string, contains only lowercase characters, or
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+ 
+
+Example 1:
+
+Input: s = "lee(t(c)o)de)"
+Output: "lee(t(c)o)de"
+Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+
+
+*/
+
+
+
+
     string minRemoveToMakeValid(string s) {
         stack<int> stac;
         string ans="";
@@ -26,4 +50,3 @@ public:
         }
         return ans;
     }
-};
